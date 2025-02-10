edc513a6baee11a70ce5ba940cbf2194a24c33dd
xkeyscore <antonin.raymondaud@gmail.com>
2025-02-10 11:43:11 +0100

    save

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,20 @@
 export class Err extends Error {
-	printError(context: string) {
+	print(context: string, plugin?: string) {
 		const timestamp = new Date().toISOString();
-
 		const red = '\x1b[31m';
 		const gray = '\x1b[90m';
 		const cyan = '\x1b[36m';
-		const yellow = '\x1b[33m';
 		const reset = '\x1b[0m';
 
 		console.log(
-			`${red}[ERROR]${reset} ${cyan}[valie]${reset} ${gray}${timestamp}${reset}` +
+			`${red}[ERROR]${reset} ${cyan}[valie${plugin ? `:${plugin}` : ""}]${reset} ${gray}${timestamp}${reset}` +
 			`\nContext: ${context}` +
 			`\nMessage: ${this.message}`
-			
 		);
 	}
 
-    constructor(context: string, message: string) {
+    constructor(context: string, message: string, plugin?: string) {
         super(message);
-        this.printError(context);
+        this.print(context, plugin);
     }
 }
